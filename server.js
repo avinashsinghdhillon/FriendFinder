d5814464a984053c9dc97e2260f6c36748468241
Avinash Singh <avinash.dhillon@gmail.com>
2019-07-04 07:47:04 -0500

    Added jumbotron to both HTML pages. Created icons for header

---
+++ server.js
@@ -0,0 +1,14 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
